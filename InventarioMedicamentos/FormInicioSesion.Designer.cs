80a131882e81f22dc4858fc1c329b421d6a58f96
AntwanGamer <antwan.cc05@gmail.com>
2025-05-15 21:40:46 -0700

    Gunaa

--- InventarioMedicamentos/FormInicioSesion.Designer.cs
+++ InventarioMedicamentos/FormInicioSesion.Designer.cs
@@ -119,10 +119,10 @@
             guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
             guna2Button1.Font = new Font("Segoe UI", 9F);
             guna2Button1.ForeColor = Color.White;
-            guna2Button1.Location = new Point(82, 297);
+            guna2Button1.Location = new Point(48, 335);
             guna2Button1.Name = "guna2Button1";
             guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            guna2Button1.Size = new Size(63, 34);
+            guna2Button1.Size = new Size(180, 45);
             guna2Button1.TabIndex = 29;
             guna2Button1.Text = "guna2Button1";
             // 
