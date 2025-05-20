67f26f53c2f8563af155a58e1e89d97284d2f46c
AntwanGamer <antwan.cc05@gmail.com>
2025-05-19 18:42:29 -0700

    Prueba diseño form inicio

---
+++ InventarioMedicamentos/FormInicioDeSesion.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace InventarioMedicamentos
+{
+    partial class FormInicioDeSesion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            panelLogin = new Guna.UI2.WinForms.Guna2Panel();
+            panelEncabezado = new Guna.UI2.WinForms.Guna2Panel();
+            labelBienvenido = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            txtUsuario = new Guna.UI2.WinForms.Guna2TextBox();
+            txtContrasena = new Guna.UI2.WinForms.Guna2TextBox();
+            btnIniciarSesion = new Guna.UI2.WinForms.Guna2Button();
+            pictureBoxLogo = new Guna.UI2.WinForms.Guna2PictureBox();
+            linkOlvidaste = new LinkLabel();
+            panelEncabezado.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // panelLogin
+            // 
+            panelLogin.CustomizableEdges = customizableEdges1;
+            panelLogin.Location = new Point(159, 140);
+            panelLogin.Name = "panelLogin";
+            panelLogin.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            panelLogin.Size = new Size(281, 338);
+            panelLogin.TabIndex = 0;
+            // 
+            // panelEncabezado
+            // 
+            panelEncabezado.Controls.Add(labelBienvenido);
+            panelEncabezado.CustomizableEdges = customizableEdges3;
+            panelEncabezado.Location = new Point(-1, -1);
+            panelEncabezado.Name = "panelEncabezado";
+            panelEncabezado.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            panelEncabezado.Size = new Size(601, 86);
+            panelEncabezado.TabIndex = 1;
+            // 
+            // labelBienvenido
+            // 
+            labelBienvenido.BackColor = Color.Transparent;
+            labelBienvenido.Location = new Point(239, 34);
+            labelBienvenido.Name = "labelBienvenido";
+            labelBienvenido.Size = new Size(97, 17);
+            labelBienvenido.TabIndex = 0;
+            labelBienvenido.Text = "guna2HtmlLabel1";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.CustomizableEdges = customizableEdges5;
+            txtUsuario.DefaultText = "";
+            txtUsuario.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtUsuario.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtUsuario.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtUsuario.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtUsuario.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtUsuario.Font = new Font("Segoe UI", 9F);
+            txtUsuario.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtUsuario.Location = new Point(22, 217);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.PlaceholderText = "";
+            txtUsuario.SelectedText = "";
+            txtUsuario.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            txtUsuario.Size = new Size(117, 27);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtContrasena
+            // 
+            txtContrasena.CustomizableEdges = customizableEdges7;
+            txtContrasena.DefaultText = "";
+            txtContrasena.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtContrasena.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtContrasena.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtContrasena.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtContrasena.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtContrasena.Font = new Font("Segoe UI", 9F);
+            txtContrasena.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtContrasena.Location = new Point(22, 250);
+            txtContrasena.Name = "txtContrasena";
+            txtContrasena.PlaceholderText = "";
+            txtContrasena.SelectedText = "";
+            txtContrasena.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            txtContrasena.Size = new Size(117, 27);
+            txtContrasena.TabIndex = 3;
+            // 
+            // btnIniciarSesion
+            // 
+            btnIniciarSesion.CustomizableEdges = customizableEdges9;
+            btnIniciarSesion.DisabledState.BorderColor = Color.DarkGray;
+            btnIniciarSesion.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnIniciarSesion.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnIniciarSesion.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnIniciarSesion.Font = new Font("Segoe UI", 9F);
+            btnIniciarSesion.ForeColor = Color.White;
+            btnIniciarSesion.Location = new Point(36, 343);
+            btnIniciarSesion.Name = "btnIniciarSesion";
+            btnIniciarSesion.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            btnIniciarSesion.Size = new Size(103, 29);
+            btnIniciarSesion.TabIndex = 4;
+            btnIniciarSesion.Text = "Iniciar Sesion";
+            btnIniciarSesion.Click += btnIniciarSesion_Click;
+            // 
+            // pictureBoxLogo
+            // 
+            pictureBoxLogo.CustomizableEdges = customizableEdges11;
+            pictureBoxLogo.ImageRotate = 0F;
+            pictureBoxLogo.Location = new Point(464, 179);
+            pictureBoxLogo.Name = "pictureBoxLogo";
+            pictureBoxLogo.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            pictureBoxLogo.Size = new Size(122, 126);
+            pictureBoxLogo.TabIndex = 5;
+            pictureBoxLogo.TabStop = false;
+            // 
+            // linkOlvidaste
+            // 
+            linkOlvidaste.AutoSize = true;
+            linkOlvidaste.Location = new Point(12, 290);
+            linkOlvidaste.Name = "linkOlvidaste";
+            linkOlvidaste.Size = new Size(141, 15);
+            linkOlvidaste.TabIndex = 6;
+            linkOlvidaste.TabStop = true;
+            linkOlvidaste.Text = "¿Olvidaste tu contraseña?";
+            // 
+            // FormInicioDeSesion
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(598, 522);
+            Controls.Add(linkOlvidaste);
+            Controls.Add(pictureBoxLogo);
+            Controls.Add(btnIniciarSesion);
+            Controls.Add(txtContrasena);
+            Controls.Add(txtUsuario);
+            Controls.Add(panelEncabezado);
+            Controls.Add(panelLogin);
+            Name = "FormInicioDeSesion";
+            Text = "FormInicioDeSesion";
+            Load += FormInicioDeSesion_Load;
+            panelEncabezado.ResumeLayout(false);
+            panelEncabezado.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel panelLogin;
+        private Guna.UI2.WinForms.Guna2Panel panelEncabezado;
+        private Guna.UI2.WinForms.Guna2TextBox txtUsuario;
+        private Guna.UI2.WinForms.Guna2TextBox txtContrasena;
+        private Guna.UI2.WinForms.Guna2Button btnIniciarSesion;
+        private Guna.UI2.WinForms.Guna2PictureBox pictureBoxLogo;
+        private LinkLabel linkOlvidaste;
+        private Guna.UI2.WinForms.Guna2HtmlLabel labelBienvenido;
+    }
+}
---
+++ InventarioMedicamentos/FormInicioDeSesion.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using InventarioMedicamentos.conexion;
+using InventarioMedicamentos.usuarios;
+using Guna.UI2.WinForms;
+
+
+namespace InventarioMedicamentos
+{
+    public partial class FormInicioDeSesion : Form
+    {
+        private usuariosConsultas usuarios;
+
+        public FormInicioDeSesion()
+        {
+            InitializeComponent();
+            InicializarInterfaz();
+            usuarios = new usuariosConsultas();
+        }
+
+        private void FormInicioDeSesion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InicializarInterfaz()
+        {
+            // Propiedades del Form
+            this.Text = "Inicio de Sesion";
+            this.Size = new Size(800, 600);
+            this.BackColor = Color.FromArgb(153, 140, 101);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // Panel Encabezado
+            panelEncabezado = new Guna2Panel();
+            panelEncabezado.FillColor = Color.FromArgb(111, 0, 42);
+            panelEncabezado.Dock = DockStyle.Top;
+            panelEncabezado.Height = 100;
+            this.Controls.Add(panelEncabezado);
+
+            // Texto Bienvenido
+            labelBienvenido = new Guna2HtmlLabel();
+            labelBienvenido.Text = "Bienvenido";
+            labelBienvenido.Font = new Font("Segoe UI", 28, FontStyle.Bold);
+            labelBienvenido.ForeColor = Color.White;
+            labelBienvenido.Dock = DockStyle.Fill;
+            labelBienvenido.TextAlignment = ContentAlignment.MiddleCenter;
+            panelEncabezado.Controls.Add(labelBienvenido);
+
+            // Panel de Login
+            panelLogin = new Guna2Panel();
+            panelLogin.FillColor = Color.LightGray;
+            panelLogin.BorderRadius = 25;
+            panelLogin.Size = new Size(350, 450);
+            panelLogin.Location = new Point((this.ClientSize.Width - panelLogin.Width) / 2, 130);
+            this.Controls.Add(panelLogin);
+
+            // Imagen logo
+            pictureBoxLogo = new Guna2PictureBox();
+            pictureBoxLogo.Image = Properties.Resources.logoIMSS.png;
+            pictureBoxLogo.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxLogo.Size = new Size(120, 120);
+            pictureBoxLogo.Location = new Point((panelLogin.Width - 120) / 2, 20);
+            panelLogin.Controls.Add(pictureBoxLogo);
+
+            // TextBox Usuario
+            txtUsuario = new Guna2TextBox();
+            txtUsuario.PlaceholderText = "Usuario";
+            txtUsuario.BorderRadius = 10;
+            txtUsuario.Size = new Size(250, 36);
+            txtUsuario.Location = new Point(50, 160);
+            panelLogin.Controls.Add(txtUsuario);
+
+            // TextBox Contraseña
+            txtContrasena = new Guna2TextBox();
+            txtContrasena.PlaceholderText = "Contraseña";
+            txtContrasena.UseSystemPasswordChar = true;
+            txtContrasena.BorderRadius = 10;
+            txtContrasena.Size = new Size(250, 36);
+            txtContrasena.Location = new Point(50, 210);
+            panelLogin.Controls.Add(txtContrasena);
+
+            // Link Olvidaste tu contraseña
+            linkOlvidaste = new LinkLabel();
+            linkOlvidaste.Text = "¿Olvidaste tu contraseña?";
+            linkOlvidaste.LinkColor = Color.Blue;
+            linkOlvidaste.AutoSize = true;
+            linkOlvidaste.Location = new Point(85, 255);
+            panelLogin.Controls.Add(linkOlvidaste);
+
+            // Botón Iniciar Sesión
+            btnIniciarSesion = new Guna2Button();
+            btnIniciarSesion.Text = "Iniciar sesión";
+            btnIniciarSesion.FillColor = Color.FromArgb(153, 112, 0);
+            btnIniciarSesion.ForeColor = Color.White;
+            btnIniciarSesion.BorderRadius = 10;
+            btnIniciarSesion.Size = new Size(250, 40);
+            btnIniciarSesion.Location = new Point(50, 290);
+            btnIniciarSesion.Click += BtnIniciarSesion_Click;
+            panelLogin.Controls.Add(btnIniciarSesion);
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            string contraseña = txtContrasena.Text;
+            string usuario = txtUsuario.Text;
+
+            if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(contraseña))
+            {
+                MessageBox.Show("Por favor, ingrese su usuario y contraseña.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            bool esAdministrador;
+            bool acceso = usuarios.IniciarSesion(usuario, contraseña, out esAdministrador);
+
+            if (acceso)
+            {
+                if (esAdministrador)
+                {
+                    MessageBox.Show("Bienvenido Administrador", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Bienvenido Usuario comun y corriente :D", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
